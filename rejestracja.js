08388bd1f6d05e7a92634c83d13820d143fca076
lukaszkwasny <lukasz.kwasny94@gmail.com>
2019-11-26 23:54:20 +0100

    aktualizacja kodu

--- rejestracja.js
+++ rejestracja.js
@@ -1,3 +1,16 @@
+var faker = require('faker');
+var email = faker.internet.email();
+var telefon = faker.random.number({min: 100000000, max: 999999999});
+var nazwisko = faker.name.lastName();
+var imie = faker.name.firstName();
+var ulica = faker.address.streetName();
+var numerdomu = faker.random.number({min: 1, max: 200});
+var numermieszkania = faker.random.number({min: 1, max: 99});
+var miejscowosc = faker.address.city();;
+var kodpocztowy = faker.address.zipCode();
+var haslo =faker.internet.password() ;
+var haslo2 = haslo;
+
 
 require('chromedriver');
 const assert = require('assert');
@@ -16,20 +29,19 @@ describe('Rejestracja', function () {
 
         await driver.findElement(By.className('link-registered')).click();
 
-        await driver.findElement(By.id('nazwisko1')).sendKeys('kowalski', Key.RETURN);
+        await driver.findElement(By.id('nazwisko1')).sendKeys(nazwisko, Key.RETURN);
 
-        await driver.findElement(By.id('imie1')).sendKeys('andrzej', Key.RETURN);
-        await driver.findElement(By.id('telefon1')).sendKeys('555', Key.RETURN);
-        await driver.findElement(By.id('ulica1')).sendKeys('grunwaldzka', Key.RETURN);
-        await driver.findElement(By.id('nr_domu1')).sendKeys('2', Key.RETURN);
-        await driver.findElement(By.id('nr_mieszkania1')).sendKeys('3', Key.RETURN);
-        await driver.findElement(By.id('miejscowosc1')).sendKeys('warszawa', Key.RETURN);
-        await driver.findElement(By.id('kod_pocztowy1')).sendKeys('55-555', Key.RETURN);
+        await driver.findElement(By.id('imie1')).sendKeys(imie, Key.RETURN);
+        await driver.findElement(By.id('telefon1')).sendKeys(telefon, Key.RETURN);
+        await driver.findElement(By.id('ulica1')).sendKeys(ulica, Key.RETURN);
+        await driver.findElement(By.id('nr_domu1')).sendKeys(numerdomu, Key.RETURN);
+        await driver.findElement(By.id('nr_mieszkania1')).sendKeys(numermieszkania, Key.RETURN);
+        await driver.findElement(By.id('miejscowosc1')).sendKeys(miejscowosc, Key.RETURN);
+        await driver.findElement(By.id('kod_pocztowy1')).sendKeys(kodpocztowy, Key.RETURN);
         await driver.findElement(By.id('uniform-przenies')).click();
-        await driver.findElement(By.id('em1')).sendKeys('lukaszssssssss@o2.pl', Key.RETURN);
-        // zmień email aby test przeszedł
-        await driver.findElement(By.id('haslo11')).sendKeys('haslo11', Key.RETURN);
-        await driver.findElement(By.id('haslo21')).sendKeys('haslo11', Key.RETURN);
+        await driver.findElement(By.id('em1')).sendKeys(email, Key.RETURN);
+        await driver.findElement(By.id('haslo11')).sendKeys(haslo, Key.RETURN);
+        await driver.findElement(By.id('haslo21')).sendKeys(haslo2, Key.RETURN);
         await driver.findElement(By.id('zgoda')).click();
         await driver.findElement(By.id('uniform-newsletter')).click();
         await driver.findElement(By.css("[class='submit-next']")).click();;
