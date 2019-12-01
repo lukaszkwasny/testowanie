d0f485002d1f34f41ab5396d263f0af8772e3dd0
lukaszkwasny <lukasz.kwasny94@gmail.com>
2019-12-01 22:56:10 +0100

    dodanie types.js

---
+++ selenium/types.js
@@ -0,0 +1,17 @@
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var NAME_INPUT = exports.NAME_INPUT = 'imie1';
+var LASTNAME_INPUT = exports.LASTNAME_INPUT = 'nazwisko1';
+var PHONE_INPUT = exports.PHONE_INPUT = 'telefon1';
+var STREET_INPUT = exports.STREET_INPUT = 'ulica1';
+var HOUSE_NUMBER_INPUT = exports.HOUSE_NUMBER_INPUT = 'nr_domu1';
+var APARTMENS_NUMBER_INPUT = exports.APARTMENS_NUMBER_INPUT = 'nr_mieszkania1';
+var ZIPCODE_INPUT = exports.ZIPCODE_INPUT = 'kod_pocztowy1';
+var PASSWORD2_INPUT = exports.PASSWORD2_INPUT = 'haslo21';
+var PASSWORD1_INPUT = exports.PASSWORD1_INPUT = 'haslo11';
+var NEWSLETTER_INPUT = exports.NEWSLETTER_INPUT = 'uniform-newsletter';
+var AGREEMENT_INPUT = exports.AGREEMENT_INPUT = 'zgoda';
+var LOCALITY_INPUT = exports.LOCALITY_INPUT = 'miejscowosc1';
+var EMAIL_INPUT = exports.EMAIL_INPUT = 'em1';
