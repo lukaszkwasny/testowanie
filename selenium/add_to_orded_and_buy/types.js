0719294d922fd8dd6f01bd38ee6e55210fb9fb01
lukaszkwasny <lukasz.kwasny94@gmail.com>
2019-12-09 18:33:39 +0100

    add file types and sklep

---
+++ selenium/add_to_orded_and_buy/types.js
@@ -0,0 +1,7 @@
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var PASSWORD1_INPUT = exports.PASSWORD1_INPUT = '//input[@id=\'passwd\']';
+var EMAIL_INPUT = exports.EMAIL_INPUT = '//input[@id=\'email\']';
